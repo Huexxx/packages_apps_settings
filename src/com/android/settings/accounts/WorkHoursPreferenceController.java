CalyxOS/platform_packages_apps_Settings

Work profile work hours feature
Allow the user to set a time at which a reminder notification to
turn off a work profile will appear every day. Tapping the notification
stops the work profile. A receiver handles both the timeout and
rescheduling the alarms at boot time.

Issue: calyxos#436
Change-Id: Ide508e651a2b1ad0044d45d1a1627c77ec80d185

src/com/android/settings/accounts/WorkHoursPreferenceController.java
@@ -1,0 +1,122 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.accounts;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.text.format.DateFormat;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.TogglePreferenceController;
+
+import java.util.Calendar;
+
+import lineageos.providers.LineageSettings;
+
+public class WorkHoursPreferenceController extends TogglePreferenceController {
+
+    private static final String TAG = "WorkHoursPreferenceController";
+
+    private Preference mPreference;
+    private UserHandle mManagedProfile;
+
+    public WorkHoursPreferenceController(Context context, String key) {
+        super(context, key);
+        UserManager userManager = context.getSystemService(UserManager.class);
+        mManagedProfile = Utils.getManagedProfile(userManager);
+    }
+
+    public void setManagedProfile(UserHandle managedProfile) {
+        mManagedProfile = managedProfile;
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return (mManagedProfile != null) ? AVAILABLE : DISABLED_FOR_USER;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        long time = LineageSettings.Secure.getLongForUser(mContext.getContentResolver(),
+                LineageSettings.Secure.USER_ACTIVITY_END_TIME, 0, mManagedProfile.getIdentifier());
+        return time != 0 ? mContext.getString(R.string.work_hours_summary,
+                DateFormat.getTimeFormat(mContext).format(time)) : null;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return LineageSettings.Secure.getLongForUser(mContext.getContentResolver(),
+                LineageSettings.Secure.USER_ACTIVITY_END_TIME, 0, mManagedProfile.getIdentifier())
+                != 0;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        if (isChecked) {
+            new TimePickerDialog(mContext,
+                    (TimePickerDialog.OnTimeSetListener) (view, hourOfDay, minute) -> {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE, minute);
+                        calendar.set(Calendar.SECOND, 0);
+                        LineageSettings.Secure.putLongForUser(mContext.getContentResolver(),
+                                LineageSettings.Secure.USER_ACTIVITY_END_TIME,
+                                calendar.getTimeInMillis(), mManagedProfile.getIdentifier());
+                        updateState(mPreference);
+                    }, 17, 0,
+                    DateFormat.is24HourFormat(mContext, mManagedProfile.getIdentifier())).show();
+        } else {
+            LineageSettings.Secure.putLongForUser(mContext.getContentResolver(),
+                    LineageSettings.Secure.USER_ACTIVITY_END_TIME, 0,
+                    mManagedProfile.getIdentifier());
+            updateState(mPreference);
+        }
+        return !isChecked;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        refreshSummary(preference);
+    }
+
+    @Override
+    protected void refreshSummary(Preference preference) {
+        if (preference != null) {
+            preference.setSummary(getSummary());
+        }
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return 0;
+    }
+}

src/com/android/settings/users/UserTimeoutReceiver.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.users;
+
+import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserManager;
+
+import com.android.settings.R;
+
+import java.util.Calendar;
+
+import lineageos.providers.LineageSettings;
+
+public class UserTimeoutReceiver extends BroadcastReceiver {
+
+    private static final String ACTION_USER_TIMEOUT = "android.intent.action.USER_TIMEOUT";
+    private static final String USER_TIMEOUT_CHANNEL = "user_timeout_channel";
+    private static final String EXTRA_NOTIFICATION = "notification";
+
+    private AlarmManager mAlarmManager;
+
+    private PendingIntent mPendingIntent;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        UserManager userManager = UserManager.get(context);
+        if (intent != null) {
+            if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+                mAlarmManager = context.getSystemService(AlarmManager.class);
+                mPendingIntent = PendingIntent.getBroadcast(context, context.getUserId(),
+                        new Intent(ACTION_USER_TIMEOUT)
+                                .setPackage(context.getPackageName()),
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+                context.getContentResolver().registerContentObserver(
+                        LineageSettings.Secure.getUriFor(
+                                LineageSettings.Secure.USER_ACTIVITY_END_TIME), false,
+                        new ContentObserver(new Handler(Looper.getMainLooper())) {
+                            @Override
+                            public void onChange(boolean selfChange) {
+                                super.onChange(selfChange);
+                                setTimeout(context);
+                            }
+                        });
+                setTimeout(context);
+            } else if (ACTION_USER_TIMEOUT.equals(intent.getAction())) {
+                if (!intent.getBooleanExtra(EXTRA_NOTIFICATION, false)) {
+                    NotificationManager notificationManager = NotificationManager.from(
+                            context);
+                    NotificationChannel notificationChannel = new NotificationChannel(
+                            USER_TIMEOUT_CHANNEL,
+                            context.getString(R.string.work_hours_title),
+                            NotificationManager.IMPORTANCE_DEFAULT);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                            context.getUserId(),
+                            new Intent(ACTION_USER_TIMEOUT).setPackage(
+                                    context.getPackageName()).putExtra(
+                                    EXTRA_NOTIFICATION, true),
+                            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+                    Notification notification = new Notification.Builder(context,
+                            USER_TIMEOUT_CHANNEL)
+                            .setSmallIcon(R.drawable.ic_settings_multiuser)
+                            .setContentTitle(
+                                    context.getString(R.string.work_hours_notification_title))
+                            .setContentText(
+                                    context.getString(R.string.work_hours_notification_description))
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true)
+                            .build();
+                    notificationManager.notify(R.drawable.ic_settings_multiuser, notification);
+                } else if (userManager.isManagedProfile()) {
+                    userManager.requestQuietModeEnabled(true, context.getUser());
+                } else {
+                    context.getSystemService(ActivityManager.class).stopUser(context.getUserId(),
+                            true);
+                }
+            }
+        }
+    }
+
+    private void setTimeout(Context context) {
+        long time = LineageSettings.Secure.getLong(context.getContentResolver(),
+                LineageSettings.Secure.USER_ACTIVITY_END_TIME, 0);
+        if (time != 0) {
+            Calendar current = Calendar.getInstance();
+            Calendar timeout = Calendar.getInstance();
+            timeout.setTimeInMillis(time);
+            timeout.set(current.get(Calendar.YEAR), current.get(Calendar.MONTH),
+                    current.get(Calendar.DAY_OF_MONTH));
+            if (current.compareTo(timeout) > 0) {
+                timeout.add(Calendar.DATE, 1);
+            }
+            time = timeout.getTimeInMillis();
+            mAlarmManager.set(
+                    AlarmManager.RTC_WAKEUP, time, AlarmManager.WINDOW_EXACT,
+                    AlarmManager.INTERVAL_DAY, mPendingIntent, null);
+        } else {
+            mAlarmManager.cancel(mPendingIntent);
+        }
+    }
+}

Huexxx/packages_apps_settings

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

src/com/android/settings/accounts/WorkHoursPreferenceController.java
@@ -1,0 +1,122 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.accounts;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.text.format.DateFormat;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.TogglePreferenceController;
+
+import java.util.Calendar;
+
+import android.provider.Settings;
+
+public class WorkHoursPreferenceController extends TogglePreferenceController {
+
+    private static final String TAG = "WorkHoursPreferenceController";
+
+    private Preference mPreference;
+    private UserHandle mManagedProfile;
+
+    public WorkHoursPreferenceController(Context context, String key) {
+        super(context, key);
+        UserManager userManager = context.getSystemService(UserManager.class);
+        mManagedProfile = Utils.getManagedProfile(userManager);
+    }
+
+    public void setManagedProfile(UserHandle managedProfile) {
+        mManagedProfile = managedProfile;
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return (mManagedProfile != null) ? AVAILABLE : DISABLED_FOR_USER;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        long time = Settings.Secure.getLongForUser(mContext.getContentResolver(),
+                Settings.Secure.USER_ACTIVITY_END_TIME, 0, mManagedProfile.getIdentifier());
+        return time != 0 ? mContext.getString(R.string.work_hours_end_on_summary,
+                DateFormat.getTimeFormat(mContext).format(time))
+                        : mContext.getString(R.string.work_hours_end_off_summary);
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.Secure.getLongForUser(mContext.getContentResolver(),
+                Settings.Secure.USER_ACTIVITY_END_TIME, 0, mManagedProfile.getIdentifier())
+                != 0;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        if (isChecked) {
+            new TimePickerDialog(mContext,
+                    (TimePickerDialog.OnTimeSetListener) (view, hourOfDay, minute) -> {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE, minute);
+                        calendar.set(Calendar.SECOND, 0);
+                        Settings.Secure.putLongForUser(mContext.getContentResolver(),
+                                Settings.Secure.USER_ACTIVITY_END_TIME,
+                                calendar.getTimeInMillis(), mManagedProfile.getIdentifier());
+                        updateState(mPreference);
+                    }, 17, 0,
+                    DateFormat.is24HourFormat(mContext, mManagedProfile.getIdentifier())).show();
+        } else {
+            Settings.Secure.putLongForUser(mContext.getContentResolver(),
+                    Settings.Secure.USER_ACTIVITY_END_TIME, 0,
+                    mManagedProfile.getIdentifier());
+            updateState(mPreference);
+        }
+        return !isChecked;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        refreshSummary(preference);
+    }
+
+    @Override
+    protected void refreshSummary(Preference preference) {
+        if (preference != null) {
+            preference.setSummary(getSummary());
+        }
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return 0;
+    }
+}

src/com/android/settings/users/UserTimeoutReceiver.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.users;
+
+import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserManager;
+
+import com.android.settings.R;
+
+import java.util.Calendar;
+
+import android.provider.Settings;
+
+public class UserTimeoutReceiver extends BroadcastReceiver {
+
+    private static final String ACTION_USER_TIMEOUT = "android.intent.action.USER_TIMEOUT";
+    private static final String USER_TIMEOUT_CHANNEL = "user_timeout_channel";
+    private static final String EXTRA_NOTIFICATION = "notification";
+
+    private AlarmManager mAlarmManager;
+
+    private PendingIntent mPendingIntent;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        UserManager userManager = UserManager.get(context);
+        if (intent != null) {
+            if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+                mAlarmManager = context.getSystemService(AlarmManager.class);
+                mPendingIntent = PendingIntent.getBroadcast(context, context.getUserId(),
+                        new Intent(ACTION_USER_TIMEOUT)
+                                .setPackage(context.getPackageName()),
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+                context.getContentResolver().registerContentObserver(
+                        Settings.Secure.getUriFor(
+                                Settings.Secure.USER_ACTIVITY_END_TIME), false,
+                        new ContentObserver(new Handler(Looper.getMainLooper())) {
+                            @Override
+                            public void onChange(boolean selfChange) {
+                                super.onChange(selfChange);
+                                setTimeout(context);
+                            }
+                        });
+                setTimeout(context);
+            } else if (ACTION_USER_TIMEOUT.equals(intent.getAction())) {
+                if (!intent.getBooleanExtra(EXTRA_NOTIFICATION, false)) {
+                    NotificationManager notificationManager = NotificationManager.from(
+                            context);
+                    NotificationChannel notificationChannel = new NotificationChannel(
+                            USER_TIMEOUT_CHANNEL,
+                            context.getString(R.string.work_hours_title),
+                            NotificationManager.IMPORTANCE_DEFAULT);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                    PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                            context.getUserId(),
+                            new Intent(ACTION_USER_TIMEOUT).setPackage(
+                                    context.getPackageName()).putExtra(
+                                    EXTRA_NOTIFICATION, true),
+                            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+                    Notification notification = new Notification.Builder(context,
+                            USER_TIMEOUT_CHANNEL)
+                            .setSmallIcon(R.drawable.ic_settings_multiuser)
+                            .setContentTitle(
+                                    context.getString(R.string.work_hours_end_notification_title))
+                            .setContentText(
+                                    context.getString(
+                                            R.string.work_hours_end_notification_description))
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true)
+                            .build();
+                    notificationManager.notify(R.drawable.ic_settings_multiuser, notification);
+                } else if (userManager.isManagedProfile()) {
+                    userManager.requestQuietModeEnabled(true, context.getUser());
+                } else {
+                    context.getSystemService(ActivityManager.class).stopUser(context.getUserId());
+                }
+            }
+        }
+    }
+
+    private void setTimeout(Context context) {
+        long time = Settings.Secure.getLong(context.getContentResolver(),
+                Settings.Secure.USER_ACTIVITY_END_TIME, 0);
+        if (time != 0) {
+            Calendar current = Calendar.getInstance();
+            Calendar timeout = Calendar.getInstance();
+            timeout.setTimeInMillis(time);
+            timeout.set(current.get(Calendar.YEAR), current.get(Calendar.MONTH),
+                    current.get(Calendar.DAY_OF_MONTH));
+            if (current.compareTo(timeout) > 0) {
+                timeout.add(Calendar.DATE, 1);
+            }
+            time = timeout.getTimeInMillis();
+            mAlarmManager.set(
+                    AlarmManager.RTC_WAKEUP, time, AlarmManager.WINDOW_EXACT,
+                    AlarmManager.INTERVAL_DAY, mPendingIntent, null);
+        } else {
+            mAlarmManager.cancel(mPendingIntent);
+        }
+    }
+}